e6ec46af2636d09dd5ae2aacc1dd1ba8653b1acd
bULLETHELL <polyswing7@hotmail.com>
2025-05-03 22:14:00 +0200

    render-markdown

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,13 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ft = { 'markdown' },
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.nvim',
+  },
+  ---@module "render-markdown"
+  ---@type render.md.UserConfig
+  opts = {
+    enabled = true,
+  },
+}
